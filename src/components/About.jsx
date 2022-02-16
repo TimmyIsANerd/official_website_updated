cc2ad8e0c53dd684224adc3fee9b786aa7a20e4c
promiselxg <okeydeede@gmail.com>
2022-02-16 20:55:29 +0100

    pull request updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,159 +8,145 @@ import Shad1 from '../assets/shad1.png';
 import Shad2 from '../assets/shad2.png';
 import Shad3 from '../assets/shad3.png';
 
-
-
 const Container = styled.div`
-margin-top: 220px;
-display: flex;
-align-items: center; 
-justify-content: space-between;
+  margin-top: 220px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
-
-
-@media screen and (max-width: 900px){
+  @media screen and (max-width: 900px) {
     flex-direction: column;
     margin-top: 50px;
-}
-`
-const Div1 = styled.div` 
-padding: 70px 72px;
-
-
-`
+  }
+`;
+const Div1 = styled.div`
+  padding: 70px 72px;
+`;
 const Div2 = styled.div`
-
-margin-top:60px;
-background-image: url(${Bg});
-background-position: top -30% right -70%;
-background-repeat: no-repeat;
-
-`
-const Con = styled.div` 
-
-
-`
+  margin-top: 60px;
+  background-image: url(${Bg});
+  background-position: top -30% right -70%;
+  background-repeat: no-repeat;
+`;
+const Con = styled.div``;
 const Shad1Con = styled.img`
-    width: 120px;
-position: relative;
+  width: 120px;
+  position: relative;
 
-
-@media screen and (max-width: 900px){
-    top: -1360px
+  @media screen and (max-width: 900px) {
+    top: -1360px;
     left: -40px;
-}
-    
-`
+  }
+`;
 const Shad2Con = styled.img`
-position: relative;
-left: 240px;
-    width: 125px;
+  position: relative;
+  left: 240px;
+  width: 125px;
 
-    @media screen and (max-width: 900px){
-left: 40px;
-}
-`
+  @media screen and (max-width: 900px) {
+    left: 40px;
+  }
+`;
 const Shad3Con = styled.img`
-    width: 120px;
-    position: relative;
-    left:140px;
+  width: 120px;
+  position: relative;
+  left: 140px;
 
-    @media screen and (max-width: 900px){
-left: -80px;
-}
-    
-`
-const  BlueCon = styled.img`
-height:310px;
-position: relative;
-left: 50px;
+  @media screen and (max-width: 900px) {
+    left: -80px;
+  }
+`;
+const BlueCon = styled.img`
+  height: 310px;
+  position: relative;
+  left: 50px;
 
-@media screen and (max-width: 900px){
-left: -160px;
-}
-`
-const  SolidCon = styled.img`
-height: 340px;
-position: relative;
-right: 80px;
+  @media screen and (max-width: 900px) {
+    left: -160px;
+  }
+`;
+const SolidCon = styled.img`
+  height: 340px;
+  position: relative;
+  right: 80px;
 
-@media screen and (max-width: 900px){
+  @media screen and (max-width: 900px) {
     top: -350px;
-left: 150px;
-}
+    left: 150px;
+  }
+`;
+const EtherCon = styled.img`
+  z-index: 90;
+  height: 220px;
+  position: relative;
+  left: 170px;
 
-`
-const  EtherCon = styled.img`
-z-index: 90;
-height: 220px;
-position: relative;
-left: 170px;
-
-@media screen and (max-width: 900px){
-left: -30px;
-}
-
-
-`
-const Ab = styled.div` 
-font-family: Poppins;
-font-style: normal;
-font-weight: bold;
-font-size: 31px;
-line-height: 150%;
-`
+  @media screen and (max-width: 900px) {
+    left: -30px;
+  }
+`;
+const Ab = styled.div`
+  font-family: Poppins;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 31px;
+  line-height: 150%;
+`;
 const BigT = styled.div`
-margin-top: 20px;
-font-family: Poppins;
-font-style: normal;
-font-weight: bold;
-font-size: 60px;
-line-height: 62px;
-/* or 103% */
+  margin-top: 20px;
+  font-family: Poppins;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 60px;
+  line-height: 62px;
+  /* or 103% */
 
-
-color: #4500A0;
-`
+  color: #4500a0;
+`;
 const SmallT = styled.div`
-margin-top: 20px;
-font-family: Poppins;
-font-style: normal;
-font-weight: normal;
-font-size: 20px;
-line-height: 35px;
+  margin-top: 20px;
+  font-family: Poppins;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 20px;
+  line-height: 35px;
 
-
-
-color: rgba(0, 0, 0, 0.7);
-
-`
+  color: rgba(0, 0, 0, 0.7);
+`;
 
 function About() {
-    return (
-        <Container>
-            <Div1>
-                <Ab>About Us</Ab>
-                <BigT>  What we are <br/> Building</BigT>
-                <SmallT> 3Swap is a decentralized exchange built on various <br/> EVM-Compatible chains that allows users to swap two <br/> tokens for one with little or no fees. Asides from the <br/> perk of swapping two tokens for one, 3Swap allows <br/> liquidity providers to earn more by allowing the <br/> provision of liquidity for three tokens.
-                </SmallT>
-            </Div1>
-            <Div2>
-               <Con>
-                                  <Shad2Con  src={Shad2}/>
+  return (
+    <Container>
+      <Div1>
+        <Ab>About Us</Ab>
+        <BigT>
+          {' '}
+          What we are <br /> Building
+        </BigT>
+        <SmallT>
+          {' '}
+          3Swap is a decentralized exchange built on various <br />{' '}
+          EVM-Compatible chains that allows users to swap two <br /> tokens for
+          one with little or no fees. Asides from the <br /> perk of swapping
+          two tokens for one, 3Swap allows <br /> liquidity providers to earn
+          more by allowing the <br /> provision of liquidity for three tokens.
+        </SmallT>
+      </Div1>
+      <Div2>
+        <Con>
+          <Shad2Con src={Shad2} />
 
-               <EtherCon src={Ether}/> 
-               <Shad3Con  src={Shad3}/>
+          <EtherCon src={Ether} />
+          <Shad3Con src={Shad3} />
 
-               <BlueCon src={Box}/> 
-               <Shad1Con  src={Shad1}/>
+          <BlueCon src={Box} />
+          <Shad1Con src={Shad1} />
 
-               <SolidCon src={Solid}/> 
-
-
-               </Con>
-            </Div2>
-        </Container>
-    )
+          <SolidCon src={Solid} />
+        </Con>
+      </Div2>
+    </Container>
+  );
 }
 
-export default About
+export default About;
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,168 +1,146 @@
 import React from 'react';
 import styled from 'styled-components';
-import Coin from '../assets/coin.png'
-import Back from '../assets/bg.png'
+import Coin from '../assets/coin.png';
+// import Back from '../assets/bg.png';
 
 const Container = styled.div`
-padding-top:70px;
+  padding-top: 70px;
 
-display: flex;
-align-items:center;
-justify-content: space-between;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
-@media screen and (max-width: 800px){
-    padding-top:30px;
+  @media screen and (max-width: 800px) {
+    padding-top: 30px;
 
     background-position: top 30% right 30%;
-
-}
-@media screen and (max-width: 700px){
+  }
+  @media screen and (max-width: 700px) {
     background-position: top 30% right 50%;
-flex-direction: column;
+    flex-direction: column;
+  }
+`;
+const Div1 = styled.div`
+  padding: 70px 72px;
 
-}
-`
-const Div1 = styled.div` 
-padding: 70px 72px;
-
-@media screen and (max-width: 900px){
+  @media screen and (max-width: 900px) {
     padding: 20px;
-}
+  }
+`;
+const BigT = styled.div`
+  font-family: Poppins;
+  font-size: 60px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 80px;
+  letter-spacing: 0em;
+  text-align: left;
 
-`
-const BigT = styled.div` 
-font-family: Poppins;
-font-size: 60px;
-font-style: normal;
-font-weight: 700;
-line-height: 80px;
-letter-spacing: 0em;
-text-align: left;
-
-br{
-
-    @media screen and (max-width: 600px){
-    display:none;
-}
-}
-@media screen and (max-width: 1300px){
+  br {
+    @media screen and (max-width: 600px) {
+      display: none;
+    }
+  }
+  @media screen and (max-width: 1300px) {
     font-size: 45px;
+  }
+  @media screen and (max-width: 800px) {
+    color: #fff;
+  }
 
-}
-@media screen and (max-width: 800px){
-    color: #FFFFF;
-}
-
-@media screen and (max-width: 900px){
+  @media screen and (max-width: 900px) {
     font-size: 35px;
     line-height: 50px;
     color: #000000;
     /* text-align: center; */
+  }
+`;
+const SmallT = styled.div`
+  margin-top: 20px;
+  font-family: Poppins;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 37px;
+  letter-spacing: 0.065em;
+  text-align: left;
 
+  br {
+    @media screen and (max-width: 600px) {
+      display: none;
+    }
+  }
 
-}
-
-`
-const SmallT = styled.div` 
-margin-top:20px;
-font-family: Poppins;
-font-size: 24px;
-font-style: normal;
-font-weight: 400;
-line-height: 37px;
-letter-spacing: 0.065em;
-text-align: left;
-
-br{
-
-@media screen and (max-width: 600px){
-display:none;
-}
-}
-
-
-@media screen and (max-width: 900px){
+  @media screen and (max-width: 900px) {
     font-size: 15px;
     line-height: 25px;
-    margin-top:10px;
+    margin-top: 10px;
+  }
+`;
+const Div2 = styled.div`
+  margin-right: 50px;
 
-}
+  @media screen and (max-width: 900px) {
+    /* display:none; */
+  }
 
-`
-const Div2 = styled.div` 
-    margin-right: 50px;
-
-
-
-@media screen and (max-width: 900px){
-/* display:none; */
-}
-
-@media screen and (max-width: 770px){
-
-margin: 0;/* height: 400px; */
-}
-}
-`
+  @media screen and (max-width: 770px) {
+    margin: 0; /* height: 400px; */
+  }
+`;
 const CoinImg = styled.img`
-
-@media screen and (max-width: 1200px){
+  @media screen and (max-width: 1200px) {
     width: 300px;
+  }
 
-}
+  @media screen and (max-width: 1024px) {
+    width: 400px;
+  }
 
-@media screen and (max-width: 1024px){
-width: 400px
-}
+  @media screen and (max-width: 800px) {
+    width: 270px;
+  }
+`;
 
-@media screen and (max-width: 800px){
-width: 270px
-}
-}
+const Btn = styled.button`
+  margin-top: 20px;
+  background: #4500a0;
+  padding: 16px 20px;
+  border-radius: 33px;
+  font-family: Poppins;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 24px;
+  /* identical to box height, or 150% */
+  color: #ffffff;
+  outline: none;
+  border: none;
+  cursor: pointer;
 
-`
-
-const Btn = styled.button` 
-margin-top: 20px;
-background: #4500A0;
-padding: 16px  20px;
-border-radius: 33px;
-font-family: Poppins;
-font-style: normal;
-font-weight: bold;
-font-size: 16px;
-line-height: 24px;
-/* identical to box height, or 150% */
-color: #FFFFFF;
-outline: none;
-border: none;
-cursor: pointer;
-
-@media screen and (max-width:900px){
-    padding: 10px  15px;
+  @media screen and (max-width: 900px) {
+    padding: 10px 15px;
     font-size: 15px;
-
-}
-`
+  }
+`;
 
 function Banner() {
   return (
     <Container>
-        <Div1>
-            <BigT>
-            One-Click, More <br/>  Swaps, Less Fees.
-            </BigT>
-            <SmallT>
-            Decentralized exchange with <br/>    extra powers. Swap two tokens <br/>for one. 
-            </SmallT>
-            <Btn>Presale Information</Btn>
-
-        </Div1>
-        <Div2>
-    <CoinImg src={Coin}/>
-        </Div2>
-
-    
+      <Div1>
+        <BigT>
+          One-Click, More <br /> Swaps, Less Fees.
+        </BigT>
+        <SmallT>
+          Decentralized exchange with <br /> extra powers. Swap two tokens{' '}
+          <br />
+          for one.
+        </SmallT>
+        <Btn>Presale Information</Btn>
+      </Div1>
+      <Div2>
+        <CoinImg src={Coin} />
+      </Div2>
     </Container>
   );
 }
